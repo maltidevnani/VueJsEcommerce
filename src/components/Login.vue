58a405df8b237daf28e36fe304b845604be830b0
Ankitkumar Brahmbhatt <abrahmbhatt4054@conestogac.on.ca>
2019-02-13 15:19:54 -0500

    Login Page Created

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,18 +1,58 @@
 <template>
-<v-app></v-app>
+<v-app>
+  <v-container >
+    <v-layout wrap justify-center align-center>
+      <v-flex md6 row >
+        <h1 class="login_title">Login Page</h1>
+           <v-text-field
+            v-model="email"
+            :rules="[rules.required, rules.email]"
+            label="E-mail">
+           </v-text-field>
+          <v-flex>
+          <v-text-field
+            v-model="password"
+            :append-icon="show1 ? 'visibility_off' : 'visibility'"
+            :rules="[rules.required, rules.min]"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            label="Password"
+            hint="At least 8 characters"
+            counter
+            @click:append="show1 = !show1"
+          ></v-text-field>
+        </v-flex>
+          <v-btn color="info">Login</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</v-app>
 </template>
-
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      title: 'Preliminary report',
+      email: '',
+      password: '',
+      show1: false,
+      rules: {
+        required: value => !!value || 'Required.',
+        counter: value => value.length <= 20 || 'Max 20 characters',
+        email(value) {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || 'Invalid e-mail.';
+        },
+        min: v => v.length >= 8 || 'Min 8 characters',
+        emailMatch: () => ('The email and password you entered don\'t match'),
+      },
+    };
+  },
+};
 </script>
 
 <style>
-.v-card--reveal {
-  align-items: center;
-  bottom: 0;
-  justify-content: center;
-  opacity: 0.5;
-  position: absolute;
-  width: 100%;
+.login_title{
+  margin-top: 100px;
 }
 </style>

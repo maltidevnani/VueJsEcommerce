0e8c818e12b24e96bc650b47f3b69d782584958a
malti <mdevnani2938@conestogac.on.ca>
2019-02-13 15:43:01 -0500

    Navigation bar changed, in cart total product added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
 <v-app>
-<v-toolbar fixed="true">
+<v-toolbar fixed="true" dark color="primary">
   <v-toolbar-side-icon></v-toolbar-side-icon>
     <v-toolbar-title class="toolbarTitleClass">One Stop Shopping Center</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat>LogIn</v-btn>
-      <v-btn flat>SignUp</v-btn>
+     <v-badge class="badgeMarginClass">
+      <span slot="badge">{{numInCart}}</span>
+      <v-icon>mdi-cart</v-icon>
+    </v-badge>
+      <v-btn flat to="/login" class="white--text">LogIn</v-btn>
+      <v-btn flat class="white--text">SignUp</v-btn>
       </v-toolbar-items>
       </v-toolbar>
   <router-view></router-view>
@@ -14,8 +18,20 @@
 </template>
 
 <script>
-export default {
+import Product from '@/components/Products';
+export default {  
   name: 'App',
+   data() {
+    return {
+    };
+  },
+  computed: {
+    inCart() { return this.$store.getters.inCart; },
+    numInCart() { return this.inCart.length; },
+  },
+  components: {
+    Product,
+  }
 };
 </script>
 
@@ -29,7 +45,11 @@ export default {
   }
 .toolbarTitleClass{
   font-family: cursive;
-  color: crimson;
+  color: white;
   font-weight: bolder;
+}
+.badgeMarginClass{
+  margin-top: 20px;
+  margin-right: 10px;
 }
 </style>

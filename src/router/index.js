4b05ec16a45071fba35559270db90510b3291b5f
malti <mdevnani2938@conestogac.on.ca>
2019-02-13 16:12:03 -0500

    Product,product detail,comments component registered

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router';
 import Payment from '@/components/Payment';
 import Profile from '@/components/Profile';
 import Products from '@/components/Products';
+import ProductDetail from '@/components/ProductDetail';
+import Comments from '@/components/Comments';
 
 Vue.use(Router);
 
@@ -17,17 +19,29 @@ export default new Router({
     {
       path: '/login',
       name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/productDetail/:Pid',
+      name: 'productDetail',
+      component: ProductDetail,
+      props: true,
+    },
+    {
+      path: '/comments/:Pid',
+      name: 'commentPage',
+      component: Comments,
+      props: true,
+    },
+    {
+      path: '/',
+      name: 'Products',
       component: Products,
     },
     {
       path: '/payment',
       name: 'Payment',
       component: Payment,
-    },
-    {
-      path: '/',
-      name: 'Products',
-      component: Profile,
     },
   ],
 });

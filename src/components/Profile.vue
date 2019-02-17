73eaf35b20142bd988dc4513a521773acce91859
Ammar Harunbhai Shama <Ashamma4021@conestogac.on.ca>
2019-02-17 10:32:55 -0500

    Added userinfo object on form submit

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -14,12 +14,37 @@
             placeholder="John Doe"
             required
           ></v-text-field>
+          <v-text-field
+            ref="email"
+            v-model="email"
+            :rules="[
+              () => !!email || 'This field is required', emailCheck]"
+              
+            label="Email"
+            placeholder="john@gmail.com"
+            counter="25"
+            required>
+            </v-text-field>
+            <v-text-field
+            ref="password"
+            v-model="password"
+            :append-icon="show1 ? 'visibility_off' : 'visibility'"
+            :rules="[() => !!password || 'This field is required']"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            label="Password"
+            hint="At least 8 characters"
+            counter
+            @click:append="show1 = !show1"
+            required>
+            </v-text-field>
            <v-text-field
             ref="address"
             v-model="address"
             :rules="[
               () => !!address || 'This field is required',
-              () => !!address && address.length <= 25 || 'Address must be less than 25 characters',
+              () => !!address && address.length <= 25 || 
+              'Address must be less than 25 characters',
               addressCheck
             ]"
             label="Address Line"
@@ -27,6 +52,7 @@
             counter="25"
             required
           ></v-text-field>
+
           <v-text-field
             ref="city"
             v-model="city"
@@ -34,31 +60,7 @@
             label="City"
             placeholder="El Paso"
             required
-          ></v-text-field><v-text-field
-            ref="state"
-            v-model="state"
-            :rules="[() => !!state || 'This field is required']"
-            label="State/Province/Region"
-            required
-            placeholder="TX"
           ></v-text-field>
-          <v-text-field
-            ref="zip"
-            v-model="zip"
-            :rules="[() => !!zip || 'This field is required']"
-            label="ZIP / Postal Code"
-            required
-            placeholder="79938"
-          ></v-text-field>
-          <v-autocomplete
-            ref="country"
-            v-model="country"
-            :rules="[() => !!country || 'This field is required']"
-            :items="countries"
-            label="Country"
-            placeholder="Select..."
-            required
-          ></v-autocomplete>
         </v-card-text>
         <v-divider class="mt-5"></v-divider>
         <v-card-actions>
@@ -91,25 +93,25 @@
 <script>
 export default {
     data: () => ({
-      countries: ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua &amp; Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia &amp; Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde', 'Cayman Islands', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D Ivoire', 'Croatia', 'Cruise Ship', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait', 'Kyrgyz Republic', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia', 'Rwanda', 'Saint Pierre &amp; Miquelon', 'Samoa', 'San Marino', 'Satellite', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St Kitts &amp; Nevis', 'St Lucia', 'St Vincent', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', "Timor L'Este", 'Togo', 'Tonga', 'Trinidad &amp; Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks &amp; Caicos', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Venezuela', 'Vietnam', 'Virgin Islands (US)', 'Yemen', 'Zambia', 'Zimbabwe'],
       errorMessages: '',
       name: null,
+      email: null,
+      password: null,
       address: null,
       city: null,
-      state: null,
-      zip: null,
-      country: null,
-      formHasErrors: false
+      formHasErrors: false,
+      show1: false,
+      counter: value => value.length <= 20 || 'Max 20 characters',
+      min: v => v.length >= 8 || 'Min 8 characters',
     }),
     computed: {
       form () {
         return {
           name: this.name,
+          email: this.email,
+          password: this.password,
           address: this.address,
           city: this.city,
-          state: this.state,
-          zip: this.zip,
-          country: this.country
         }
       }
     },
@@ -119,6 +121,13 @@ export default {
       }
     },
     methods: {
+      passwordCheck(value){
+       return value => value.length >= 8 || 'Min 8 characters';
+      },
+      emailCheck(value) {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || 'Invalid e-mail.';
+        },
       addressCheck () {
         this.errorMessages = this.address && !this.name
           ? 'Hey! I\'m required'
@@ -134,9 +143,17 @@ export default {
       },
       submit () {
         this.formHasErrors = false;
+        console.log("form", this.form);
         Object.keys(this.form).forEach(f => {
-        if (!this.form[f]) this.formHasErrors = true;
+        if (!this.form[f]){
+        this.formHasErrors = true;
         this.$refs[f].validate(true);
+        }else{
+         this.$store.commit('signUp', this.form);
+         this.$router.push({ name: 'Login'});
+        }
+        
+        
       });
     },
   },

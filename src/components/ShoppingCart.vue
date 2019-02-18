d7bc53dd2abf4a5437abb4b9d2e0dfd10c04c6e5
Ankitkumar Brahmbhat <Abrahmbhatt4054@conestogac.on.ca>
2019-02-17 23:51:07 -0500

    Added view to shopping cart

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,14 +1,47 @@
 <template>
-    <v-badge class="badgeMarginClass">
-      <span slot="badge">{{numInCart}}</span>
-      <v-icon>mdi-cart</v-icon>
-    </v-badge>
+  <div>
+    <table class="table">
+            <tbody>
+              <tr v-for="item in cart" :key="item.id">
+                <td>{{ item.title }}</td>
+                <td>{{ item.quantity }}</td>
+              </tr>
+            </tbody>
+          </table>
+    <button class="btn btn-primary" data-toggle="modal" data-target="#shoppingCart">Cart ({{ numInCart }})</button>
+    <div id="shoppingCart" class="modal fade">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Shopping cart</h5>
+            <button class="close" data-dismiss="modal">
+              &times;
+            </button>
+          </div>
+          <div class="modal-body">
+            
+          </div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" data-dismiss="modal">Keep shopping</button>
+            <button class="btn btn-primary">Check out</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
     computed: {
-    inCart() { return this.$store.getters.inCart; },
-    numInCart() { return this.inCart.length; },
+      inCart() { return this.$store.getters.inCart; },
+      numInCart() { return this.inCart.length; },
+      cart() {
+        return this.$store.getters.inCart.map((cartItem) => {
+          return this.$store.getters.getProductData.find((forSaleItem) => {
+            return cartItem === forSaleItem.id;
+          });
+        });
+      },
   },
 }
 </script>
